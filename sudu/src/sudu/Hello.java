ee572f92c6c573350b8a7a744f409e5b2046f23d
unknown <sudug1008@gmail.com>
2022-07-22 16:45:18 +0530

    MyFirst

---
+++ sudu/src/sudu/Hello.java
@@ -0,0 +1,11 @@
+package sudu;
+
+public class Hello 
+{
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ sudu/src/sudu/rsa.java
@@ -0,0 +1,5 @@
+package sudu;
+
+public class rsa {
+
+}
